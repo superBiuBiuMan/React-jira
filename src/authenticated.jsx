a46f4d2558f33880f397c62534b689c1d2eefa74
qiuye <1464815392@qq.com>
2023-02-05 15:23:18 +0800

    feat: 1.创建Row组件，并设置已认证页面信息头部样式

--- src/authenticated.jsx
+++ src/authenticated.jsx
@@ -2,15 +2,39 @@ import React from 'react';
 import ProjectList from "./pages/projectList";
 import {useAuth} from "./context/authContext";
 import {Button} from "antd";
+import styled from "@emotion/styled";
+import Row from "../src/component/lib";
 /*已认证页面信息*/
 const Authenticated = () => {
     const {loginOut} = useAuth();
     return (
-        <div>
-            <Button onClick={() => loginOut()}>退出登录</Button>
-            <ProjectList/>
-        </div>
+        <>
+            {/*头部*/}
+            <Header between = {true} marginBottom={'1'}>
+                {/*左侧*/}
+                <HeaderLeft gap = {true}>
+                    <h3>Logo</h3>
+                    <h3>Logo</h3>
+                    <h3>Logo</h3>
+                </HeaderLeft>
+                {/*右侧*/}
+                <HeaderRight>
+                    <Button onClick={() => loginOut()}>退出登录</Button>
+                </HeaderRight>
+            </Header>
+            {/*主要内容*/}
+            <Main>
+                <ProjectList/>
+            </Main>
+        </>
     );
 };
 
 export default Authenticated;
+const Header = styled(Row)``;
+const HeaderLeft = styled(Row)``;
+const HeaderRight = styled(Row)``;
+
+/*主要内容样式*/
+const Main = styled.div``
+
