8e76055fe253fdb722468f6a724fff9b24751df9
qiuye <1464815392@qq.com>
2023-02-20 21:15:10 +0800

    feat: 1.实现Id-Select解决Id难题

---
+++ src/component/idSelect.tsx
@@ -0,0 +1,31 @@
+import {Options, Raw} from "../types/types";
+import {Select} from "antd";
+import {SelectProps} from "antd/es/select";
+export type OnChange = (value?:number) => void;
+
+
+export interface IdSelectProps extends Omit<SelectProps,"value" | "onChange" | "options">{
+  value: Raw | null | undefined,
+  onChange:OnChange
+  defaultOptionName?:string,
+  options?:Options[]
+}
+
+export const IdSelect = (props:IdSelectProps) => {
+  const {value,onChange,defaultOptionName,options,...originProps} = props;
+  return (
+      <Select value={toNumber(value)} onChange={(value) => onChange(toNumber(value) || undefined)} {...originProps}>
+        {/* 默认项 */}
+        {
+          defaultOptionName ? <Select.Option value={0}>{ defaultOptionName }</Select.Option> : null
+        }
+        {
+          options?.map(item => (
+              <Select.Option key={item.id} value={item.id}>{ item.name }</Select.Option>
+          ))
+        }
+      </Select>
+  )
+}
+
+export const toNumber = (value:unknown) => isNaN(Number(value)) ? 0 : Number(value);
---
+++ src/types/types.ts
@@ -0,0 +1,6 @@
+export type Raw = string | number;
+
+export interface Options {
+  name:string,
+  id:number,
+}
